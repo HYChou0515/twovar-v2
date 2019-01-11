3e50413bd857d3f50f2688e826e7dbe1abd9630c
Chou Hung-Yi <s1243221@gmail.com>
2019-01-11 18:07:57 +0800

    as for covtype_scale C=32, SemiGD+random is as bad as origin SemiGD, I suspect it's because r is too small, so I use more r

--- code/script/expconf.py
+++ code/script/expconf.py
@@ -8,21 +8,21 @@ runtype = [
 	"ONE_L1_SEMIGD_RAND_1000",
 	"ONE_L1_SEMIGD_DUALOBJ_1000",
 	"ONE_L1_SEMIGD_DUALOBJ_RAND_1000",
-	"ONE_L1_CY_1000",
-	"ONE_L1_RD_1000",
-	"ONE_L1_CY_SH",
-	"ONE_L1_RD_SH",
+	#"ONE_L1_CY_1000",
+	#"ONE_L1_RD_1000",
+	#"ONE_L1_CY_SH",
+	#"ONE_L1_RD_SH",
 ]
 nlist = [0.1]
-clist = [1.0/32, 32]
+clist = [32]
 elist = [1e-12]
 rlist = [
 	#1,0.99,0.98,0.97,0.96,0.95,0.94,0.93,0.92,0.91,0.9,0.8,0.7,0.6,0.5,0.1,0.001
 	#1,0.95,0.9,0.5,0.1,0.01,0.001
 	#1,2,4,8,16,32
-	0.1, 0.3,
-	0.5,
-	#0.9,0.99
+	#0.1, 0.3,
+	#0.5,
+	0.6,0.7,0.8,0.9,0.99
 	#1,0.95,0.9,0.5,0.1,0.001
 	#,0.5
 	#0.2, 0.1
@@ -31,15 +31,15 @@ rlist = [
 m = 100000
 timeout = 7200
 dataset = [
-	"heart_scale",
-	"a9a",
-	"ijcnn1",
-	"rcv1_train.binary",
-	"real-sim",
-	"news20.binary",
-	"yahoojp",
+	#"heart_scale",
+	#"a9a",
+	#"ijcnn1",
+	#"rcv1_train.binary",
+	#"real-sim",
+	#"news20.binary",
+	#"yahoojp",
 	"covtype.libsvm.binary.scale",
-	"yahookr",
+	#"yahookr",
 	]
 
 
