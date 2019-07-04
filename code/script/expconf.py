d4ab73033e920d1b64c2cf38a68f2c7bf7143fa9
Chou Hung-Yi <s1243221@gmail.com>
2019-07-04 23:51:42 +0800

    exp ocsvm/svdd with semigd2-dualobj

--- code/script/expconf.py
+++ code/script/expconf.py
@@ -4,8 +4,16 @@ resumefolder='resume'
 runtype = [
 		## FOR EXP: 2-cd vs new-4 vs semigd vs semigd-rd
 		"STUB",
+		"ONECLASS_L1_CY_1000",
+		"ONECLASS_L1_SEMIGD_CY_FIRST_1000", #r=4
+		"ONECLASS_L1_SEMIGD_CY_DUALOBJ_1000", #r=4
+		"ONECLASS_L1_SEMIGD_1000", #r=0.1
+		"ONECLASS_L1_SEMIGD_RAND_1000", #r=0.1
+		## FOR EXP: 2-cd vs new-4 vs semigd vs semigd-rd
+		"STUB",
 		"SVDD_L1_CY_1000",
 		"SVDD_L1_SEMIGD_CY_FIRST_1000", #r=4
+		"SVDD_L1_SEMIGD_CY_DUALOBJ_1000", #r=4
 		"SVDD_L1_SEMIGD_1000", #r=0.1
 		"SVDD_L1_SEMIGD_RAND_1000", #r=0.1
 ]
@@ -47,18 +55,14 @@ MIN_SQUASH = 0.2
 YLIM = (1e-9, 1e20)
 
 uselabel = {
-		20112: "no-bias-cd-sh",
-		40612: "bias-cd-sh",
-		40412: "bias-perm-greedy-sh",
-		20122: "no-bias-cd-sh",
-		40622: "bias-cd-sh",
-		40422: "bias-perm-greedy-sh",
-		50812: "perm-cd-sh",
-		50612: "perm-greedy-sh",
-		50212: "semi-greedy-sh",
-		50512: "semi-greedy-random-sh",
+		50811: "perm-cd",
+		50611: "perm-greedy",
+		50911: "perm-greedy-2nd",
+		50211: "semi-greedy",
+		50511: "semi-greedy-random",
 		60811: "perm-cd",
 		60611: "perm-greedy",
+		60911: "perm-greedy-2nd",
 		60211: "semi-greedy",
 		60511: "semi-greedy-random",
 }
