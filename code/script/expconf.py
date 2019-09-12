4e07b2f3b34a3e840b58b04cca4caed132166f9b
Chou Hung-Yi <s1243221@gmail.com>
2019-09-12 16:09:32 +0800

    exp for ocsvm 3level semigd

--- code/script/expconf.py
+++ code/script/expconf.py
@@ -3,9 +3,9 @@ resumefolder='resume'
 
 runtype = [
 		## FOR EXP: 2-cd vs new-4 vs semigd vs semigd-rd
-		"SVDD_L1_CY_1000",
-		"SVDD_L1_SEMIGD_CY_FIRST_1000", #r=4
-		"SVDD_L1_SEMIGD_1000", #r=0.1
+#		"SVDD_L1_CY_1000",
+#		"SVDD_L1_SEMIGD_CY_FIRST_1000", #r=4
+#		"SVDD_L1_SEMIGD_1000", #r=0.1
 
 		"ONECLASS_L1_CY_1000",
 		"ONECLASS_L1_SEMIGD_CY_FIRST_1000", #r=4
@@ -24,19 +24,19 @@ clist = [
 elist = [1e-2]
 rlist = [
 	4,
-#	0.1
+	0.1
 ]
 m = 300000
 timeout = 50400
 tolerance = 1e-10
 dataset = [
-	"yahookr",
-	"yahoojp",
-	"covtype.libsvm.binary.scale",
-	"a9a",
-	"ijcnn1",
-	"rcv1_train.binary",
-	"real-sim",
+#	"yahookr",
+#	"yahoojp",
+#	"covtype.libsvm.binary.scale",
+#	"a9a",
+#	"ijcnn1",
+#	"rcv1_train.binary",
+#	"real-sim",
 	"news20.binary",
 	]
 
