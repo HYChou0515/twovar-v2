5ce443daab2fbd2b46c0d5f28f6d770b4d2ba02a
Chou Hung-Yi <s1243221@gmail.com>
2019-12-14 10:33:24 +0800

    adjust expconf

--- code/script/expconf.py
+++ code/script/expconf.py
@@ -4,14 +4,22 @@ resumefolder='resume'
 runtype = [
 		## FOR EXP: 2-cd vs new-4 vs semigd vs semigd-rd
 		"SVDD_L1_CY_1000",
+		"SVDD_L1_FIRST_1000",
 		"SVDD_L1_SEMIGD_CY_FIRST_1000", #r=4
 		"SVDD_L1_SEMIGD_1000", #r=0.1
 
-#		"ONECLASS_L1_CY_1000",
-#		"ONECLASS_L1_SEMIGD_CY_FIRST_1000", #r=4
-#		"ONECLASS_L1_SEMIGD_1000", #r=0.1
+		"ONECLASS_L1_CY_1000",
+		"ONECLASS_L1_FIRST_1000",
+		"ONECLASS_L1_SEMIGD_CY_FIRST_1000", #r=4
+		"ONECLASS_L1_SEMIGD_1000", #r=0.1
 ]
-nlist = [0.2,0.1,0.05,0.01,0.005,0.001,0.0001]
+nlist = [
+		#0.2,
+		0.1,
+		#0.05,
+		0.01,0.005,
+		#0.001,0.0001
+		]
 clist = []
 elist = [1e-2]
 rlist = [
@@ -39,13 +47,15 @@ PROCESS_MAX = 1
 MARKER = ["--",]
 Y_BUFFER = 2
 MIN_SQUASH = 0.2
-YLIM = (1e-9, 1000)
+YLIM = (1e-6, 1000)
 
 uselabel = {
 		50811: "cyclic-2cd",
+		50311: "greedy-2cd",
 		50611: "cyclic-%scd-greedy",
 		50211: "greedy-%s-cyclic",
 		60811: "cyclic-2cd",
+		60311: "greedy-2cd",
 		60611: "cyclic-%scd-greedy",
 		60211: "greedy-%s-cyclic",
 }
